1559aba01b582d11018af042bdede8b615758879
Antek Szadaj <antek.szadaj@cern.ch>
2017-10-13 13:13:34 +0200

    refinements
    Former-commit-id: 545b8712e08be1dcf762e1c38ed4bf594349dd76

--- noise_distribution.py
+++ noise_distribution.py
@@ -10,7 +10,7 @@ import numpy as np
 # Create TFile object from .root-file
 # NB! Use the correct file-path
 #f = ROOT.TFile("/eos/user/k/kastanas/TB/osci_conv/data_1504818689.tree.root")
-f = ROOT.TFile("~/data_1504818689.tree.root")
+f = ROOT.TFile("~/cernbox/oscilloscope_data/data_1504818689.tree.root")
 
 # get the TTree from the TFile
 t  = f.Get("oscilloscope")
@@ -18,18 +18,18 @@ t  = f.Get("oscilloscope")
 # Sampling period of the scope
 dt = 0.1
 
-# Nested tuple, three dimensional
+# Data points, for each entry, there are 8 channels, with noise points
+# E.g. data[0][1][2] is for entry 0, channel 1 and point 2
 data = []
 
 # loop over entries
-for e in range(1,t.GetEntries()): #t.GetEntries()
+for e in range(0,40000): #t.GetEntries()
     
     data.append([])
-    t.GetEntry(e-1)
+    t.GetEntry(e)
     
     # Loop over all channels
     for chan in [t.chan0, t.chan1, t.chan2, t.chan3, t.chan4, t.chan5, t.chan6, t.chan7]:
-        
         # Convert ROOT-array to numpy-array and set in mV
         chan_np = np.asarray(chan,'d')*1000
         
@@ -39,50 +39,44 @@ for e in range(1,t.GetEntries()): #t.GetEntries()
         # Put all points until the pulse, that is below 25 mV.
         for point in chan_np:
             if abs(point) < 25:
-                points_condition = np.append(points_condition,point)
+                points_condition.append(point)
             else:
-                data[e-1].append(points_condition[:-3])
                 break
+    
+        # Removing three points to make the collection smoother.
+        data[e].append(points_condition[:-3])
 
 # maybe remove some points before the pulse?
 # are there channels which give two pulses?
 # are there differences between the pulses
 
 pedestal = []
-stddevs = []
-
 bins = 100 # Convention to see a better resolution
-c2 = ROOT.TCanvas("Histograms", "Histograms") # Dictionary for drawing
-entry = 1
-histogram = ""
-for e in data:
-    for chan in e:
-        if len(chan)!=0:
-            # Defining structure of histogram
-            min1 = min(chan)*0.9
-            max1 = max(chan)*1.1
-            histogram = ROOT.TH1D("Histograms","Histograms",bins,min1,max1)
+
+histogram = ROOT.TH1D("Histograms","Histograms",1,-1,1)
+
+for entry in data:
+    for channel in entry:
             
-            # Sorting the channel-list to use the distribution function
-            chan.sort()
-            for i in chan:
-                histogram.Fill(i)
+        # Defining structure of histogram
+        min_bin = min(channel)*0.9
+        max_bin = max(channel)*1.1
+        histogram.SetBins(bins,min_bin,max_bin)
         
-            # Draw and fit a Gauss distribution function with <x> and sigma
-            histogram.Draw()
-            pedestal.append(histogram.GetMean())
-            stddevs.append(histogram.GetStdDev())
+        # Sorting the channel-list to use the distribution function
+        channel.sort()
+        for i in channel:
+            histogram.Fill(i)
+    
+        pedestal.append(histogram.GetMean())
 
-    entry += 1
 
-
-bins = 100 # Convention to see a better resolution
 c = ROOT.TCanvas("Distributions", "Distributions") # Dictionary for drawing
 
-# Defining structure of histogram
-min1 = min(pedestal)*0.9
-max1 = max(pedestal)*1.1
-histogram = ROOT.TH1D("Pedestal","Pedestal",bins,min1,max1)
+# Define attributes for the histogram
+min_bin = min(pedestal)*0.9
+max_bin = max(pedestal)*1.1
+histogram = ROOT.TH1D("Pedestal","Pedestal",bins,min_bin,max_bin)
 histogram.SetLineColor(1)
 histogram.SetMarkerColor(1)
 histogram.GetYaxis().SetTitle("Amount in each bin (N)")
@@ -90,12 +84,12 @@ histogram.GetXaxis().SetTitle("Pedestal (mV)")
 setTitleAboveGraph = "Pedestal distribution for all entries"
 histogram.SetTitle(setTitleAboveGraph)
 
-# Sorting and fill the histogram
+# Sort and fill TH1D - histogram
 pedestal.sort()
 for i in pedestal:
     histogram.Fill(i)
 
-# Draw and fit a Gauss distribution function with <x> and sigma
+# Draw and fit with a Gaus curve
 histogram.Draw()
 histogram.Fit("gaus")
 pedestal_total = histogram.GetMean()
@@ -111,5 +105,3 @@ c.Update()
 c.Print(file_name)
 
 
-
-    
