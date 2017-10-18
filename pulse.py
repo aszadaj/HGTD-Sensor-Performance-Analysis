b28f9ac30f209af11bcaaf401ea0e54d1c5d8340
Antek Szadaj <antek.szadaj@cern.ch>
2017-10-18 12:16:16 +0200

    pulse, stable version
    Former-commit-id: 547aa30573624eeab9f1355a97a65ae0e13cdef0

--- pulse.py
+++ pulse.py
@@ -3,73 +3,117 @@ import numpy as np
 import root_numpy as rnm
 import json
 
+# The code obtains amplitudes and risetime for pulses for each channel for all selected entries
+# and orders them in a nested list within "amplitudes" and "risetime".
 def main():
-    end = raw_input ("Until which entry? (0-200 000) or m as max: ")
+    first_entry = raw_input ("From which entry? (0-220 000) or m as max: ")
+    last_entry = 0
+    if first_entry != "m":
+        last_entry = raw_input ("Until which entry? (0-220 000) or l as last: ")
+    
     dataFileName = "~/cernbox/oscilloscope_data/data_1504818689.tree.root"
     pulsePropertiesFileName = "pedestal_noise.json"
     
-    data,channels = setUpData(dataFileName,end)
+    data,channels = setUpData(dataFileName,first_entry,last_entry)
     
-    pedestal,noise = importInfo(pulsePropertiesFileName)
+    pedestal,noise = importNoiseProperties(pulsePropertiesFileName)
     
-    amplitudes = findAmplitudeForPulse(data,pedestal,noise)
+    amplitudes,risetime = getPulseInfo(data,pedestal,noise)
 
-def findAmplitudeForPulse(data,pedestal,noise):
-    dt = 0.1
+    print "\n"
+    for entry in range(0,len(data)):
+        for chan in channels:
+            if amplitudes[chan][entry] != 0:
+                print "Entry: " + str(int(first_entry)+entry) + " Channel: " + str(chan) +  "\nAmplitude: " +str(amplitudes[chan][entry]) +" mV\nRisetime: "+ str(risetime[chan][entry]) +" ns \n"
+
+# Creates amplitudes and risetime dictionaries and goes through all entries
+# and channels. Catches exceptions for KeyErrors
+def getPulseInfo(data,pedestal,noise):
+    
     amplitudes = dict()
-    for chan in data.dtype.names:
-        amplitudes[chan] = np.empty(0)
-    eventNumber = 0
+    risetime = dict()
+  
     for event in data:
         
         for chan in data.dtype.names:
-            event[chan] = event[chan]*1000-pedestal[chan] # Convert from V to mV and remove the pedestal
-            noise_limit = -25 # Set 25 mV limit on the noise oscillation curve
-            amplitude_value = np.amin(event[chan])
-            amplitude_index = np.argmin(event[chan])
             
+            try:
+                amplitudes[chan]
+            except Exception, e:
+                amplitudes[chan] = np.empty(0)
             
-            # How to extract the noise more exactly?
-            # The STD for noise is about 2 up to 5 mV,
-            # which is not in accordance with data (which is at least 5 mV)
+            try:
+                risetime[chan]
+            except Exception, e:
+                risetime[chan] = np.empty(0)
+        
+            amplitude_entry_channel,risetime_entry_channel = getRisetimeAndAmplitude(event[chan],pedestal[chan])
             
-            # Problem with defining where the pulse starts.
-            # amplitude_places takes too many values which are irrelevant
-            
-            if  amplitude_value < noise_limit:
-                amplitude_places = np.argwhere((event[chan][0:amplitude_index]>0.9*amplitude_value) & (event[chan][0:amplitude_index]<0.1*amplitude_value))
-                print amplitude_places
-                risetime = (np.amax(amplitude_places)-np.amin(amplitude_places))*dt
-                print "event: " + str(eventNumber) + " channel: " + str(chan) + " risetime: " + str(risetime) + " min value: " + str(amplitude_value) + " index: " + str(amplitude_index)
-                
-              
-                amplitudes[chan] = np.append(amplitudes[chan],amplitude_value)
-                
+            amplitudes[chan] = np.append(amplitudes[chan],amplitude_entry_channel)
+            risetime[chan] = np.append(risetime[chan],risetime_entry_channel)
 
-        eventNumber+=1
-    return amplitudes
+    return amplitudes, risetime
 
-def setUpData(dataFileName,end):
+# Obtains amplitude and risetime values for a entry and channel.
+# If no pulse found, the result will be 0 for both amplitude and risetime.
+# NB! This code assumes that there is only one pulse per entry.
+def getRisetimeAndAmplitude (event, pedestal):
+    
+    dt = 0.1 # Time scope, in ns
+    
+    # Set condition on finding the pulse, and select indices where the pulse is.
+    points_condition = event<-25*0.001
+    pulse_first_index = np.where(points_condition)[0][0] if len( np.where(points_condition)[0] ) else 1002
+    pulse_last_index = np.argmin(event)
+    
+    event = event*1000-pedestal # Convert from V to mV and remove the pedestal
+    noise_limit = -25+pedestal # Set 25 mV limit on the noise oscillation curve
+    
+    pulse_amplitude = np.amin(event)
+    pulse_risetime = 0
+    
+    # How to extract the noise more exactly?
+    # The STD for noise is about 2 up to 5 mV,
+    # which is not in accordance with data (which is at least 5 mV)
+  
+  
+    if  pulse_amplitude < noise_limit:
+        
+        # Select indices which are between 10% and 90% of the pulse.
+        amplitude_indices = np.argwhere((event[pulse_first_index:pulse_last_index]>0.9*pulse_amplitude) & (event[pulse_first_index:pulse_last_index]<0.1*pulse_amplitude))
+        pulse_risetime = (amplitude_indices[-1][0]-amplitude_indices[0][0])*dt
+    
+    else:
+        pulse_amplitude = 0
+    
+    return pulse_amplitude, pulse_risetime
+
+
+def setUpData(dataFileName,first,last):
     
     data = ""
     
-    if end == "m":
+    if first == "m":
         data = rnm.root2array(dataFileName)
+    elif last == "l":
+        data = rnm.root2array(dataFileName,start=int(first))
     else:
-        data = rnm.root2array(dataFileName,stop=int(end))
+        data = rnm.root2array(dataFileName,start=int(first),stop=int(last))
 
     channels = data.dtype.names # gets names for the leafs in TTree file
     return data, channels
 
-def importInfo(fileName):
+
+def importNoiseProperties(fileName):
     # load from file:
     with open(fileName, 'r') as f:
         try:
             data = json.load(f)
         # if the file is empty the ValueError will be thrown
         except ValueError:
+            print "The file  " + str(fileName) + " is empty or cannot be read."
             data = {}
 
-    return data["p"],data["n"]
+    return data["p"], data["n"]
 
 main()
