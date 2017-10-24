a0a6fb81fbd549d15891ad52d33226e4832defc7
Antek Szadaj <antek.szadaj@cern.ch>
2017-10-24 21:08:56 +0200

    all entries
    Former-commit-id: efe3b9e78eef08d1d76437ed936ebf91a2be5617 [formerly 70edfe693ac405e0b8bc377cf48fa6cc5ff4266f]
    Former-commit-id: 1a4e6f6513f5fa76ef9d894ed7562853d26a84f5

--- pulse.py
+++ pulse.py
@@ -13,14 +13,16 @@ def main():
     data, sensors, first_entry, last_entry = importFileData()
     
     amplitudes, risetimes = getPulseInfoForAllEntriesAndChannels(data)
-    
-    produceDistributionPlots(amplitudes, risetimes, data.dtype.names, sensors)
-    
-    exportInfo(amplitudes, risetimes,data.dtype.names)
-   
+ 
+    produceDistributionPlots(amplitudes, risetimes, sensors)
+  
+    exportInfo(amplitudes, risetimes)
+  
     exit()
 
-# Creates amplitudes and risetime dictionaries and goes through all entries and channels.
+# Creates dictionaries for amplitudes and rise time where each key defines the channel and for each key
+# there is a list of obtained amplitudes, where each index corresponds to an entry.
+# E.g amplitudes["chan1"][233]: amplitude value for channel chan0 and entry 233
 def getPulseInfoForAllEntriesAndChannels(data):
     
     channels = data.dtype.names
@@ -28,37 +30,41 @@ def getPulseInfoForAllEntriesAndChannels(data):
     
     amplitudes = dict()
     risetimes = dict()
+    criticalValue = dict()
+    
     
     for chan in channels:
         
         amplitudes[chan] = np.empty(0)
         risetimes[chan] = np.empty(0)
-   
-   
+        criticalValue[chan] = findCriticalValuePerChannel(data[chan])
+       
     for entry in range(0,len(data)):
         
         for chan in channels:
             
-            amplitude_entry_channel, risetime_entry_channel = getRisetimeAndAmplitude(data[entry][chan], pedestal[chan], chan, entry)
+            convert_mV = 1000 # V to mV
+            
+            amplitude_entry_channel, risetime_entry_channel = getRisetimeAndAmplitude(data[entry][chan]*convert_mV, pedestal[chan], chan, entry, criticalValue[chan])
             
             amplitudes[chan] = np.append(amplitudes[chan], amplitude_entry_channel)
+            
             risetimes[chan] = np.append(risetimes[chan], risetime_entry_channel)
-
 
     return amplitudes, risetimes
 
 
-# Obtains amplitude and risetime values for a entry and channel. Also obtains how many pulses cannot be calculated
-# If no pulse found, the result will be 0 for both amplitude and risetime.
-# NB1! This code assumes that there is only one pulse per entry.
-# NB2! Fitting minimal value by evaluating points is usally not the best way, it
-# decreases in realiablity, one could use instead a distribution fit
-def getRisetimeAndAmplitude (event, pedestal, chan, entry):
+# Calculates amplitude and rise time for selected entry and channel. It returns a zero value if:
+# 1. It is a critical value
+# 2. It is not sufficiently high (for now, set as -35 mV
+# 3. Cannot be calculated, due to insufficient points, also too low.
+def getRisetimeAndAmplitude (event, pedestal, chan, entry, criticalValue):
     
     dt = 0.1 # Time scope, in ns
+    noise_limit = -20 # Limit to select amplitudes above the noise level
     
     # Set condition on finding the pulse, and select indices where the pulse is.
-    indices_condition = event<-20*0.001
+    indices_condition = event < noise_limit
     
     pulse_amplitude = 0
     pulse_risetime = 0
@@ -68,33 +74,105 @@ def getRisetimeAndAmplitude (event, pedestal, chan, entry):
         pulse_first_index = np.where(indices_condition)[0][0] - 3
         pulse_last_index = np.argmin(event)
         
-        event = event*1000 # Convert from V to mV
-        pulse_limit = -40-pedestal # Set 40 mV to consider it as a pulse
+        pulse_amplitude = np.amin(event)
         
-        pulse_amplitude = np.amin(event)-pedestal
+        pulse_limit = -35 # Limit to select sufficiently high amplitudes
         
-        criticalValue = -354.7959327-pedestal
-        criticalValue_chan3 = -383.29595327-pedestal
-        
-        if  pulse_amplitude < pulse_limit and pulse_amplitude > criticalValue_chan3:
+        if criticalValue < pulse_amplitude < pulse_limit:
             
-            if chan != "chan3" and pulse_amplitude < criticalValue:
+            # Select indices which are between 10% and 90% of the pulse.
+            amplitude_indices = np.argwhere((event[pulse_first_index:pulse_last_index]>0.9*pulse_amplitude) & (event[pulse_first_index:pulse_last_index]<0.1*pulse_amplitude))
+            
+            try:
+                pulse_risetime = (amplitude_indices[-1][0] - amplitude_indices[0][0])*dt
+            except Exception, e:
                 pulse_amplitude = 0
-            
-            else:
-                
-                # Select indices which are between 10% and 90% of the pulse.
-                amplitude_indices = np.argwhere((event[pulse_first_index:pulse_last_index]>0.9*pulse_amplitude) & (event[pulse_first_index:pulse_last_index]<0.1*pulse_amplitude))
-                
-                try:
-                    pulse_risetime = (amplitude_indices[-1][0] - amplitude_indices[0][0])*dt
-                except Exception, e:
-                    pulse_amplitude = 0
-
+                pulse_risetime = 0
+        
         else:
             pulse_amplitude = 0
-    
+
+    if pulse_amplitude != 0:
+        pulse_amplitude -= pedestal
+
     return pulse_amplitude, pulse_risetime
+
+
+def findCriticalValuePerChannel(channelData):
+
+    criticalValue = 0
+
+    for entry in channelData:
+
+        if criticalValue > np.amin(entry):
+            criticalValue = np.amin(entry)
+                
+    return criticalValue*1000
+
+
+# Create 8 plots, for each channel across all entries, for amplitudes and risetime
+def produceDistributionPlots(amplitudes,risetimes,sensors):
+    
+    channels = amplitudes.keys()
+    
+    canvas_amplitude = ROOT.TCanvas("amplitude", "Amplitude Distribution")
+    canvas_risetime = ROOT.TCanvas("risetime", "Risetime Distribution")
+
+    amplitude_graph = dict()
+    risetime_graph = dict()
+    
+    for chan in channels:
+        
+        lowBin = 0
+        
+        if chan == "chan3":
+            lowBin = -384
+        else:
+            lowBin = -355
+        
+        amplitude_graph[chan] = ROOT.TH1D("amplitude_"+chan,"Amplitude "+chan,800,lowBin,0)
+        risetime_graph[chan] = ROOT.TH1D("risetime_"+chan,"Risetime "+chan,50,0,1)
+        
+        index = int(chan[-1:])
+        
+        for entry in range(0,len(amplitudes[chan])):
+            if amplitudes[chan][entry] != 0 and risetimes[chan][entry] != 0:
+                amplitude_graph[chan].Fill(amplitudes[chan][entry])
+                risetime_graph[chan].Fill(risetimes[chan][entry])
+
+        typeOfGraph = "amplitude"
+        defineAndProduceHistogram(amplitude_graph[chan],canvas_amplitude,typeOfGraph,sensors[index],chan)
+
+        typeOfGraph = "risetime"
+        defineAndProduceHistogram(risetime_graph[chan],canvas_risetime,typeOfGraph,sensors[index],chan)
+
+
+# Produce TH1 plots and export them as a PDF file
+def defineAndProduceHistogram(graphList,canvas,typeOfGraph,sensor,chan):
+    
+    xAxisTitle = "Time (ns)"
+    yAxisTitle = "Number (N)"
+    fileName = "pulse_distributions/rise_time_distribution_"+chan+".pdf"
+    titleAbove = "Distribution of rise times, oscilloscope for Sep 2017 Run 3656, for channel " + chan + ", sensor: " + str(sensor)
+    
+    if typeOfGraph == "amplitude":
+    
+        xAxisTitle = "Amplitude (mV)"
+        fileName = "pulse_distributions/amplitude_distribution_"+chan+".pdf"
+        titleAbove = "Distribution of amplitudes, oscilloscope for Sep 2017 Run 3656, for channel " + chan + ", sensor: " + str(sensor)
+
+    graphList.SetLineColor(1)
+    graphList.SetMarkerColor(1)
+    graphList.GetYaxis().SetTitle(yAxisTitle)
+    graphList.GetXaxis().SetTitle(xAxisTitle)
+    
+    setTitleAboveGraph = titleAbove
+    graphList.SetTitle(setTitleAboveGraph)
+    
+    canvas.cd()
+    graphList.Draw()
+    canvas.Update()
+    canvas.Print(fileName)
 
 
 # Prompt for choosing entry 
@@ -116,7 +194,8 @@ def importFileData():
         last_entry = 200263
 
     dataFileName = "~/cernbox/SH203X/HGTD_material/oscilloscope_data_sep_2017/data_1504818689.tree.root"
-
+    
+    # Note, these names are specific for the file specified above
     sensors = ["W9-LGA35","50D-GBGR2","W4-LG12","SiPM-AFP","W4-S215","W4-S215","W4-S215","W4-S215"]
     
     data = rnm.root2array(dataFileName, start=int(first_entry), stop=int(last_entry))
@@ -144,109 +223,15 @@ def importNoiseProperties():
     return data["p"]
 
 
-# Create 8 plots, for each channel across all entries, for amplitudes and risetime
-def produceDistributionPlots(amplitudes,risetimes,channels,sensors):
-    
-    canvas_amplitude = ROOT.TCanvas("amplitude", "Amplitude Distribution")
-    canvas_risetime = ROOT.TCanvas("risetime", "Risetime Distribution")
-    
-    index = 0
-    
-    yAxisLimits_amplitude = [250,250,350,1200,200,200,200,200]
-    
-    amplitude_graph = dict()
-    risetime_graph = dict()
-    
-    for chan in channels:
-        amplitude_graph[chan] = ROOT.TH1D("amplitude_"+chan,"Amplitude "+chan,1000,-400,0)
-        risetime_graph[chan] = ROOT.TH1D("risetime_"+chan,"Risetime "+chan,100,0,1)
-    
-    
-    for chan in channels:
-        
-        for entry in range(0,len(amplitudes[chan])):
-            if amplitudes[chan][entry] != 0:
-                amplitude_graph[chan].Fill(amplitudes[chan][entry])
-                risetime_graph[chan].Fill(risetimes[chan][entry])
-        
-        titleAbove = "Distribution of amplitudes, oscilloscope for Sep 17 Run 3656, for channel " + chan + ", sensor: " + str(sensors[index])
-        xAxisTitle = "Amplitude (mV)"
-        yAxisTitle = "Number (N)"
-        
-        setGraphAttributes(amplitude_graph[chan],titleAbove,xAxisTitle,yAxisTitle)
-        
-        amplitude_graph[chan].SetMaximum(yAxisLimits_amplitude[index])
-        
-        titleAbove = "Distribution of rise times, oscilloscope for Sep 17 Run 3656, for channel " + chan + ", sensor: " + str(sensors[index])
-        xAxisTitle = "Time (ns)"
-        yAxisTitle = "Number (N)"
-        setGraphAttributes(risetime_graph[chan],titleAbove,xAxisTitle,yAxisTitle)
-        
-        fileName = "pulse_distributions/amplitude_distribution_"+chan+".pdf"
-        exportGraph(amplitude_graph[chan],canvas_amplitude,fileName)
-        
-        fileName = "pulse_distributions/risetime_distribution_"+chan+".pdf"
-        exportGraph(risetime_graph[chan],canvas_risetime,fileName)
 
-        index += 1
-
-
-# Define TH1 properties
-def setGraphAttributes(graphList,titleAbove,xAxisTitle,yAxisTitle):
+# Export dictionaries amplitude and risetime and list of channels in a .pkl file
+def exportInfo(amplitude,risetime):
     
-    graphList.SetLineColor(1)
-    graphList.SetMarkerColor(1)
-    graphList.GetYaxis().SetTitle(yAxisTitle)
-    graphList.GetXaxis().SetTitle(xAxisTitle)
-    
-    setTitleAboveGraph = titleAbove
-    graphList.SetTitle(setTitleAboveGraph)
-
-
-# Draw the graph and export it as PDF
-def exportGraph(graphList,canvas,fileName):
-    
-    canvas.cd()
-    graphList.Draw()
-    canvas.Update()
-    canvas.Print(fileName)
-
-
-def exportInfo(amplitude,risetime,channels):
-    
-    with open("amplitude_risetime.pkl","wb") as output:
+    with open("amplitude_rise_time.pkl","wb") as output:
         
         pickle.dump(amplitude,output,pickle.HIGHEST_PROTOCOL)
         
         pickle.dump(risetime,output,pickle.HIGHEST_PROTOCOL)
-
-        pickle.dump(channels,output,pickle.HIGHEST_PROTOCOL)
-
-
-def printGraph(data_points,entry,channel):
-
-    canvas = ROOT.TCanvas("Waveforms", "Waveforms")
-    
-    dt = 0.1
-
-    graph = ROOT.TGraph(len(data_points))
-    graph.SetLineColor(1)
-    graph.SetMarkerColor(1)
-    
-    for i in range(0,len(data_points)):
-        graph.SetPoint(i, i*dt, data_points[i])
-
-    drawOpt = "LPA"
-    graph.Draw(drawOpt)
-    graph.GetYaxis().SetRangeUser(min(data_points)*1.2, max(data_points)*2)
-    graph.Draw(drawOpt)
-
-    canvas.cd()
-    canvas.Update()
-    plotName ="graphs/plot_entry" + str(entry) +"_"+str(channel) +".pdf"
-    canvas.Print(plotName)
-
-
 
 
 
