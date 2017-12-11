d19fc5fc0b50ee245114551e41c422cb28b31682
Antek Szadaj <antek.szadaj@cern.ch>
2017-12-11 13:05:10 +0100

    upload stau
    Former-commit-id: d448ae032dd7a1275941ef9253151a27c032deca [formerly cf10d02da0b155965bee683eb32b2a631b513d85]
    Former-commit-id: 7bc35ba726753196592ed55f2960d7d122bf6194

--- pulse_calculations.py
+++ pulse_calculations.py
@@ -121,6 +121,8 @@ def removeUnphyscialQuantities(results, noise, sigma):
         amplitudes[chan][indices] = 0
         rise_times[chan][indices] = 0
         half_max_times[chan][indices] = 0
+        print len(pulse_points[chan])
+        print np.sum(pulse_points[chan][np.where(pulse_points[chan] != -1)])
         fraction_del_amplitudes[chan] = np.sum(pulse_points[chan][np.where(pulse_points[chan] != -1)])
 
     return [convertData(amplitudes), rise_times, half_max_times, convertData(criticalValues), pulse_points, fraction_del_amplitudes]
