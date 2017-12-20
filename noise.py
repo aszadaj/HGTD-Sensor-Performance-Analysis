c66eea1413800f831420a02d004343ca13a0c36d
Antek Szadaj <antek.szadaj@cern.ch>
2017-12-20 16:28:23 +0100

    upload stau
    Former-commit-id: ea02facf059922967eb4afca177fca0852fb4e81 [formerly f6245542bdc48656475ee1f0429630f3f790ff5e]
    Former-commit-id: 2ec4cf2197c0ceca993a7fe3b7e9ec3832a8bd61

--- noise.py
+++ noise.py
@@ -75,14 +75,14 @@ def noiseAnalysisPerRun():
     startTime = md.getTime()
     
 #    # Configure inputs for multiprocessing
-#    p = Pool(dm.threads)
-#    max = 200000 # Restrict to match the file of the telescope
-#    step = 5000
+    p = Pool(dm.threads)
+    max = 200000 # Restrict to match the file of the telescope
+    step = 5000
 
 #    # DEBUG #
-    p = Pool(1)
-    max = 1000 # Restrict to match the file of the telescope
-    step = 1000
+#    p = Pool(1)
+#    max = 1000 # Restrict to match the file of the telescope
+#    step = 1000
 
     ranges = range(0, max, step)
     
