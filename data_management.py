changed charge calculation and fixed smaller bug with exporting of timing resolution files

data_management.py
@@ -206,10 +206,10 @@ def readFileNames(group, category=""):
     
     if group == "oscilloscope":
         
-        dataPath = getSourceFolderPath() + getOscillscopeSourceFolder() + "/"
+        dataPath = getOscillscopeSourceFolder() + "/"
 
     else:
-        dataPath = getSourceFolderPath() + getDataSourceFolder() + "/" + group + "/" + category + "/"
+        dataPath = getDataSourceFolder() + "/" + group + "/" + category + "/"
 
     # This line reads in a list of sorted files
     # from a provided folder above, strips down non-integer characters and converts to integers.
@@ -314,6 +314,16 @@ def getDTYPETrackingPosition():
     return np.dtype([('chan0', '<f8', 2), ('chan1', '<f8', 2) ,('chan2', '<f8', 2) ,('chan3', '<f8', 2) ,('chan4', '<f8', 2) ,('chan5', '<f8', 2) ,('chan6', '<f8', 2) ,('chan7', '<f8', 2)])
 
 
+def positionFileExists():
+    
+    check = exportImportROOTData("tracking", "position")
+
+    if check.size == 1:
+        return False
+    else:
+        return True
+
+
 # Get actual time
 def getTime():
 
@@ -352,7 +362,6 @@ def getResultsPlotSourceDataPath():
     return resultsPlotSourceDataPath
 
 
-# Return path of data files
 def getSourceFolderPath():
     
     return sourceFolderPath
@@ -375,7 +384,6 @@ def defMainDirectories(directories):
     resultsPlotSourceDataPath = directories[5]
 
 
-
 def setFunctionAnalysis(function):
 
     global functionAnalysis
@@ -384,6 +392,6 @@ def setFunctionAnalysis(function):
 
 def getOscilloscopeFilePath():
 
-    dataPath = getSourceFolderPath() + getOscillscopeSourceFolder() + "/" + "data_"+str(md.getTimeStamp())+".tree.root"
+    dataPath = getOscillscopeSourceFolder() + "/" + "data_"+str(md.getTimeStamp())+".tree.root"
 
     return dataPath

pulse_calculations.py
@@ -19,17 +19,16 @@ def getPulseCharacteristics(data, signal_limit, threshold_points):
     noise, pedestal = calculateNoiseAndPedestal(data)
     threshold = N * noise + pedestal
     
-    # points and max_sample are calculated without the threshold point condition
-    # to analyze how many points are required
+    # Calculate number of data samples and max sample value above threshold.
     points = calculatePoints(data, threshold)
     max_sample = np.amax(data[data > threshold]) if np.sum(data > threshold) != 0 else 0
-    
+
 
     if points >= threshold_points:
         
         pulse_amplitude, peak_time = calculatePeakValue(data, pedestal, signal_limit)
         rise_time, cfd  = calculateRiseTime(data, pedestal)
-        charge = calculateCharge(data, threshold)
+        charge = calculateCharge(data, pedestal)
         
 
         # Condition: if the time locations are not in synch, disregard those
@@ -44,12 +43,13 @@ def getPulseCharacteristics(data, signal_limit, threshold_points):
         return noise, -pedestal, 0, 0, 0, 0, 0, points, -max_sample
 
 
+
 def calculateNoiseAndPedestal(data):
 
     # Select samples above threshold
     samples_bool = data > threshold_noise
     
-    # If that is low, ignore the event (this is checked, happens for ~0.03 % of all events)
+    # If that is low, ignore the event (~0.03 % of all events)
     if np.sum(samples_bool) > 0 and np.where(samples_bool)[0][0] <= data_point_correction:
     
         std = 0
@@ -68,9 +68,12 @@ def calculateNoiseAndPedestal(data):
         std = np.std(data)
         avg = np.average(data)
     
+    # In case of 'corrupted' samples, set them to zero (< 0.0001% of all events)
+    if np.isnan(std) or np.isnan(avg):
+    
+        std = avg = 0
 
     return std, avg
-
 
 
 # Calculate the pulse amplitude value
@@ -153,13 +156,15 @@ def calculateRiseTime(data, pedestal, graph=False):
 
 
 # Calculate the charge over the threshold
-def calculateCharge(data, threshold):
-
-    # transimpendence is the same for all sensors, except for W4-RD01, which is unknown
-    transimpendence = 4700
-    voltage_integral = np.trapz(data[data > threshold], dx = timeScope)*10**(-9)
-    charge = voltage_integral / transimpendence
+def calculateCharge(data, pedestal):
     
+    # transimpendence is the same for all sensors, except for W4-RD01, which is unknown
+    transimpendence = 4700.0
+    args_pedestal = np.argwhere(data > pedestal).flatten()
+    selection = getConsecutiveSeries(data, args_pedestal)
+    voltage_integral = np.trapz(data[selection], dx = timeScope)*10**(-9)
+    charge = voltage_integral / transimpendence
+
     return charge
 
 
@@ -167,32 +172,30 @@ def calculateCharge(data, threshold):
 def calculatePoints(data, threshold):
 
     points_threshold = np.argwhere(data > threshold).flatten()
-    points_consecutive = getConsecutiveSeriesPoints(data, points_threshold)
+    points_consecutive = getConsecutiveSeries(data, points_threshold)
     
     return len(points_consecutive)
 
 
 # Select consecutive samples for a range of data set
-def getConsecutiveSeriesPoints(data, points_threshold):
-    
-    if len(points_threshold) == 0:
-        return []
-
-    group_points = group_consecutives(points_threshold)
-    group_points_max = [np.amax(data[group]) for group in group_points]
-    max_arg_series = group_points[group_points_max.index(max(group_points_max))]
-    
-    return max_arg_series
-
-
-# Select consecutive samples which have the maximum height
-def getConsecutiveSeries(data):
+def getConsecutiveSeries(data, limit_arguments=False):
     
     if len(data) == 0:
         return []
 
-    group_points = group_consecutives(data)
-    group_points_max = [np.amax(group) for group in group_points]
+    # For the case of linear fit selection
+    if len(data) < 1002:
+
+        group_points = group_consecutives(data)
+        group_points_max = [np.amax(group) for group in group_points]
+
+    elif not np.any(limit_arguments):
+        return []
+    
+    else:
+        group_points = group_consecutives(limit_arguments)
+        group_points_max = [np.amax(data[group]) for group in group_points]
+
     max_arg_series = group_points[group_points_max.index(max(group_points_max))]
     
     return max_arg_series

pulse_plot.py
@@ -46,7 +46,6 @@ def pulsePlots():
     print "Done with producing PULSE plots.\n"
 
 
-# Fill TH1 objects
 def producePulsePlots(numpy_variables):
 
     [noise, pedestal, pulse_amplitude, rise_time, charge, cfd, peak_time, points, max_sample] = [i for i in numpy_variables]
@@ -69,18 +68,24 @@ def producePulsePlots(numpy_variables):
             continue
 
         point_count_limit = 50
-        charge_pulse_bins = 150
+        charge_pulse_bins = 140
         rise_time_bins = 300
+        charge_max = 150
         
         # This is a limit for the point count, to increase it
         if md.getSensor() == "SiPM-AFP" or md.getSensor() == "W4-RD01":
             point_count_limit = 100
+            charge_max = 500
+
         
         print md.getSensor(), "\n"
+
+        noise_avg_std    = [np.average(noise[chan]), np.std(noise[chan])]
+        pedestal_avg_std = [np.average(pedestal[chan]), np.std(pedestal[chan])]
         
-        noise_avg_std, pedestal_avg_std = getAvgStd(noise, pedestal)
         noise_ranges, pedestal_ranges = getRanges(noise_avg_std, pedestal_avg_std, 6)
         
+ 
         # Create TH objects with properties to be analyzed
         
         th_name = "_"+str(md.getBatchNumber())+"_"+md.chan_name
@@ -90,7 +95,7 @@ def producePulsePlots(numpy_variables):
         
         pulse_amplitude_TH1F                = ROOT.TH1F("pulse_amplitude"+th_name, "pulse_amplitude", 200, 0, 500)
         rise_time_TH1F                      = ROOT.TH1F("rise_time"+th_name, "rise_time", rise_time_bins, 0, 4000)
-        charge_TH1F                         = ROOT.TH1F("charge"+th_name, "charge", charge_pulse_bins, 0, 500)
+        charge_TH1F                         = ROOT.TH1F("charge"+th_name, "charge", charge_pulse_bins, 0, charge_max)
         
         # Additional TH objects for inspection of signals
         peak_time_TH1F                      = ROOT.TH1F("peak_time"+th_name, "peak_time", 100, 0, 100)
@@ -114,10 +119,10 @@ def producePulsePlots(numpy_variables):
                 max_sample_vs_points_threshold_TH2F.Fill(points[entry][chan], max_sample[entry][chan])
 
 
-        # Create fits for pulse amplitude, rise time and charge
         # Redefine ranges for noise and pedestal fits
         ranges_noise_pedestal = getRanges(noise_avg_std, pedestal_avg_std, 3)
-        
+
+        # Create fits
         noise_fit, pedestal_fit = makeNoisePedestalFits(noise_TH1F, pedestal_TH1F, ranges_noise_pedestal)
         pulse_amplitude_langaufit = makeLandauGausFit(pulse_amplitude_TH1F, signal_limit)
         rise_time_fit = makeRiseTimeFit(rise_time_TH1F)
@@ -129,8 +134,6 @@ def producePulsePlots(numpy_variables):
 
         exportHistogram(max_sample_vs_points_threshold_TH2F)
 
-        del noise_TH1F, pedestal_TH1F, pulse_amplitude_TH1F, rise_time_TH1F, charge_TH1F, peak_time_TH1F, cfd_TH1F, point_count_TH1F, max_sample_TH1F
-
 
 def makeNoisePedestalFits(noise_TH1F, pedestal_TH1F, ranges):
     
@@ -140,38 +143,38 @@ def makeNoisePedestalFits(noise_TH1F, pedestal_TH1F, ranges):
     return noise_TH1F.GetFunction("gaus"), pedestal_TH1F.GetFunction("gaus")
 
 
-def makeRiseTimeFit(graphList):
+def makeRiseTimeFit(th1_object):
 
     # Change window for rise time values
-    bin_max_rise_time = graphList.GetMaximumBin()
-    max_value_rise_time = graphList.GetBinCenter(bin_max_rise_time)
+    bin_max_rise_time = th1_object.GetMaximumBin()
+    max_value_rise_time = th1_object.GetBinCenter(bin_max_rise_time)
     window_range = 1000
     xMin = max_value_rise_time - window_range
     xMax = max_value_rise_time + window_range
-    graphList.SetAxisRange(xMin, xMax)
+    th1_object.SetAxisRange(xMin, xMax)
 
     # Select range for the fit for rise time plot
-    xMin = max_value_rise_time - graphList.GetStdDev()
-    xMax = max_value_rise_time + graphList.GetStdDev()
-    graphList.Fit("gaus", "Q", "", xMin, xMax)
+    xMin = max_value_rise_time - th1_object.GetStdDev()
+    xMax = max_value_rise_time + th1_object.GetStdDev()
+    th1_object.Fit("gaus", "Q", "", xMin, xMax)
 
-    return graphList.GetFunction("gaus")
+    return th1_object.GetFunction("gaus")
 
 
-def makeLandauGausFit(graphList, signal_limit=0):
+def makeLandauGausFit(th1_object, signal_limit=0):
     
-    if graphList.GetTitle() == "charge":
-        xMax = graphList.GetXaxis().GetXmax()
+    if th1_object.GetTitle() == "charge":
+        xMax = th1_object.GetXaxis().GetXmax()
     
     else:
         xMax = signal_limit
-        graphList.SetAxisRange(0, xMax)
+        th1_object.SetAxisRange(0, xMax)
     
     # Create parameters for landau (*) gaus fit for CHARGE distribution
-    std_dev = graphList.GetStdDev()
-    integral = graphList.Integral()
-    MPV_bin = graphList.GetMaximumBin()
-    MPV = graphList.GetBinCenter(MPV_bin)
+    std_dev = th1_object.GetStdDev()
+    integral = th1_object.Integral()
+    MPV_bin = th1_object.GetMaximumBin()
+    MPV = th1_object.GetBinCenter(MPV_bin)
     
     # Set range for fit
     xMin = max((MPV - std_dev), 0)
@@ -188,24 +191,12 @@ def makeLandauGausFit(graphList, signal_limit=0):
     param_limits_low = np.multiply(np.array([std_dev, MPV, integral, std_dev]), 0.1)
     param_limits_high = np.multiply(param_limits_low, 100)
 
-    langauFit = ROOT.langaufit(graphList, fit_range, start_values, param_limits_low, param_limits_high)
+    langauFit = ROOT.langaufit(th1_object, fit_range, start_values, param_limits_low, param_limits_high)
 
-    if graphList.GetTitle() == "pulse_amplitude":
-        graphList.SetAxisRange(0, 500)
+    if th1_object.GetTitle() == "pulse_amplitude":
+        th1_object.SetAxisRange(0, 500)
 
     return langauFit
-
-
-
-def getAvgStd(noise_std, noise_average):
-    
-    noise_avg   = np.average(noise_std[md.chan_name][np.nonzero(noise_std[md.chan_name])])
-    noise_std  = np.std(noise_std[md.chan_name][np.nonzero(noise_std[md.chan_name])])
-    
-    pedestal_avg = np.average(noise_average[md.chan_name][np.nonzero(noise_average[md.chan_name])])
-    pedestal_std  = np.std(noise_average[md.chan_name][np.nonzero(noise_average[md.chan_name])])
-
-    return [noise_avg, noise_std], [pedestal_avg, pedestal_std]
 
 
 def getRanges(noise_avg_std, pedestal_avg_std, N):
@@ -218,32 +209,32 @@ def getRanges(noise_avg_std, pedestal_avg_std, N):
 
 
 # Produce histograms
-def exportHistogram(graphList):
+def exportHistogram(th1_object):
 
     canvas.Clear()
-    category = graphList.GetTitle()
+    category = th1_object.GetTitle()
     titles = getPlotAttributes(category)
     drawOpt = "COLZ"
     
     if category != "max_sample_vs_point_count":
         ROOT.gStyle.SetOptStat("ne")
         ROOT.gStyle.SetOptFit(0012)
-        graphList.SetLineColor(1)
+        th1_object.SetLineColor(1)
         drawOpt = ""
     
-    graphList.SetTitle(titles[0])
-    graphList.GetXaxis().SetTitle(titles[1])
-    graphList.GetYaxis().SetTitle(titles[2])
+    th1_object.SetTitle(titles[0])
+    th1_object.GetXaxis().SetTitle(titles[1])
+    th1_object.GetYaxis().SetTitle(titles[2])
 
-    graphList.Draw(drawOpt)
+    th1_object.Draw(drawOpt)
     canvas.Update()
     
     if category == "max_sample_vs_point_count":
     
-        graphList.SetAxisRange(0, 500, "Z")
+        th1_object.SetAxisRange(0, 500, "Z")
     
         # Redefine the stats box
-        stats_box = graphList.GetListOfFunctions().FindObject("stats")
+        stats_box = th1_object.GetListOfFunctions().FindObject("stats")
         stats_box.SetX1NDC(0.65)
         stats_box.SetX2NDC(0.9)
         stats_box.SetY1NDC(0.93)
@@ -252,7 +243,7 @@ def exportHistogram(graphList):
 
 
     canvas.Print(titles[3])
-    dm.exportImportROOTHistogram("pulse", category, "", "", graphList)
+    dm.exportImportROOTHistogram("pulse", category, "", "", th1_object)
 
 
 def getPlotAttributes(category):

tracking_plot.py
@@ -71,10 +71,10 @@ def trackingAnalysis():
                 numpy_arrays[index] = np.concatenate((numpy_arrays[index], np.take(dm.exportImportROOTData(var_names[index][0], var_names[index][1]), np.arange(0, len(tracking_run)))), axis=0)
 
             numpy_arrays[-1] = np.concatenate((numpy_arrays[-1], tracking_run), axis=0)
-        
-        
-        # This function can be turned on once, to export the center positions for each pad.
-        t_calc.calculateCenterOfSensorPerBatch(numpy_arrays[0], numpy_arrays[-1])
+    
+        # This checks if the position file exists, otherwise it will create it for future reference. This is done once for whole batch 10X etc
+        if not dm.positionFileExists():
+            t_calc.calculateCenterOfSensorPerBatch(numpy_arrays[0], numpy_arrays[-1])
                         
         trackingPlots(numpy_arrays)
                         

waveform_debug.py
@@ -20,7 +20,7 @@ batch = 102
 sensor = "W9-LGA35"
 
 # If event = 0, then the event number will be randomly selected based on condition
-event = 0
+event = 151999
 number_of_plots = 3
 
 # Print selected event for sensor and batch
@@ -39,7 +39,7 @@ def printWaveform(batchNumber, sensor, event = 0):
     
     # Import properties to be studied
     rise_time_import = dm.exportImportROOTData("pulse", "rise_time")
-    peak_value_import = dm.exportImportROOTData("pulse", "peak_value")
+    peak_value_import = dm.exportImportROOTData("pulse", "pulse_amplitude")
     max_sample = dm.exportImportROOTData("pulse", "max_sample")
     peak_time_import = dm.exportImportROOTData("pulse", "peak_time")
     points_import = dm.exportImportROOTData("pulse", "points")
@@ -49,7 +49,7 @@ def printWaveform(batchNumber, sensor, event = 0):
     if event == 0:
     
         # Here one can modify the condition which randomly selects an event. Event number must be 0
-        condition = peak_value_import[chan] < -0.35
+        condition = charge_import[chan] > 10 * 10**-15
         selected_event =  np.argwhere(condition).flatten()
         np.random.shuffle(selected_event)
         event = selected_event[0]
@@ -69,9 +69,9 @@ def printWaveform(batchNumber, sensor, event = 0):
     signal_limit_DUT = 0.3547959
     point_difference = 2
     
-    rise_time, cfd, linear_fit, linear_fit_indices = p_calc.calculateRiseTime(data, pedestal, True)
     peak_value, peak_time, poly_fit = p_calc.calculatePeakValue(data, pedestal, signal_limit_DUT, True)
-    charge = p_calc.calculateCharge(data, threshold)
+    rise_time, cfd, linear_fit, linear_fit_indices = p_calc.calculateRiseTime(data, pedestal, True)
+    charge = p_calc.calculateCharge(data, pedestal)
     point_count = p_calc.calculatePoints(data, threshold)
     max_sample = np.amax(data) - pedestal
 
@@ -91,29 +91,30 @@ def printWaveform(batchNumber, sensor, event = 0):
     graph_10 = ROOT.TGraph(2)
     graph_90 = ROOT.TGraph(2)
     graph_pedestal = ROOT.TGraph(2)
+    graph_noise = ROOT.TGraph(2)
     graph_linear_fit = ROOT.TGraph(len(linear_fit_indices))
     graph_2nd_deg_fit = ROOT.TGraph(point_difference*2+1)
 
     # Define points for all selected values
     
     # Fill shade between threshold and waveform
-    n = np.sum((data > threshold)) - 1
+    # This one is adapted for Batch 102, W9-LGA35, event 151999
+    n = np.sum((data > pedestal) & (np.nonzero(data)[0] > 431) & (np.nonzero(data)[0] < 452))+1
     charge_fill = ROOT.TGraph(2*n)
+    fillOnce = True
 
-    i = 0
-    j = 0
     for index in range(0, len(data)):
-        graph_waveform.SetPoint(i,index*0.1, data[index]*1000)
         
-        # This is a function filling the pulse, but has to be adapted depending on the event.
-        # This one is adapted for Batch 102, W9-LGA35, event 151999
-#        if data[index] > threshold and (400 < index < 450):
-#            charge_fill.SetPoint(j, index*0.1, data[index]*1000)
-#            charge_fill.SetPoint(n+j, (index+n-j)*0.1, data[index+n-j]*1000)
-#            j+=1
+        graph_waveform.SetPoint(index, index*0.1, data[index]*1000)
+  
+        if index > 431 and fillOnce:
+            
+            for i in range(0, n):
 
-        i+=1
-        
+                charge_fill.SetPoint(i,   0.1 * (i+index),     data[i+index] * 1000)
+                charge_fill.SetPoint(n+i, 0.1 * (n-i+index-1), pedestal * 1000)
+            
+            fillOnce = False
 
     # Plot based on 5 points
     first_index = np.argmax(data) - point_difference
@@ -138,6 +139,12 @@ def printWaveform(batchNumber, sensor, event = 0):
     # Set points to draw lines
     graph_threshold.SetPoint(0,0, threshold*1000)
     graph_threshold.SetPoint(1,1002, threshold*1000)
+
+    graph_noise.SetPoint(0,0, noise*1000)
+    graph_noise.SetPoint(1,1002, noise*1000)
+
+    graph_pedestal.SetPoint(0,0, pedestal*1000)
+    graph_pedestal.SetPoint(1,1002, pedestal*1000)
     
     graph_peak_value.SetPoint(0,0, peak_value*1000)
     graph_peak_value.SetPoint(1,1002, peak_value*1000)
@@ -150,9 +157,7 @@ def printWaveform(batchNumber, sensor, event = 0):
 
     graph_peak_time.SetPoint(0, peak_time, -30)
     graph_peak_time.SetPoint(1, peak_time, 500)
-    
-    graph_pedestal.SetPoint(0,0, pedestal)
-    graph_pedestal.SetPoint(1,1002, pedestal)
+
     
     graph_10.SetPoint(0,0, peak_value*0.1*1000)
     graph_10.SetPoint(1,1002, peak_value*0.1*1000)
@@ -165,6 +170,7 @@ def printWaveform(batchNumber, sensor, event = 0):
     graph_2nd_deg_fit.SetLineWidth(3)
     graph_waveform.SetMarkerStyle(6)
     graph_waveform.SetLineColor(2)
+    graph_cfd.SetLineStyle(7)
 
     
     graph_2nd_deg_fit.SetLineColor(3)
@@ -173,6 +179,7 @@ def printWaveform(batchNumber, sensor, event = 0):
     graph_cfd.SetLineColor(8)
     graph_peak_time.SetLineColor(8)
     graph_pedestal.SetLineColor(6)
+    graph_noise.SetLineColor(7)
     graph_threshold.SetLineColor(1)
 
     graph_max_sample.SetLineColor(2)
@@ -190,13 +197,14 @@ def printWaveform(batchNumber, sensor, event = 0):
 
     # Add the graphs to multigraph
     multi_graph.Add(graph_waveform)
+    multi_graph.Add(graph_noise)
     multi_graph.Add(graph_threshold)
     multi_graph.Add(graph_2nd_deg_fit)
     multi_graph.Add(graph_linear_fit)
     multi_graph.Add(graph_peak_value)
-    multi_graph.Add(graph_max_sample)
-    multi_graph.Add(graph_10)
-    multi_graph.Add(graph_90)
+    #multi_graph.Add(graph_max_sample)
+    #multi_graph.Add(graph_10)
+    #multi_graph.Add(graph_90)
     multi_graph.Add(graph_cfd)
     multi_graph.Add(graph_peak_time)
     multi_graph.Add(graph_pedestal)
@@ -205,17 +213,18 @@ def printWaveform(batchNumber, sensor, event = 0):
     
     # Add the information to a legend box
     legend.AddEntry(graph_waveform, "Waveform " + md.getSensor(), "l")
-    legend.AddEntry(graph_peak_value, "Pulse amplitude: "+str(peak_value[0]*1000)[:5]+" mV", "l")
-    legend.AddEntry(graph_linear_fit, "Rise time: "+str(rise_time*1000)[:5]+" ps", "l")
-    legend.AddEntry(graph_cfd, "CFD: " + str(cfd)[0:4] + " ns", "l")
-    legend.AddEntry(graph_peak_time, "Time at peak: " + str(peak_time[0])[0:4] + " ns", "l")
+    legend.AddEntry(graph_noise, "Noise: "+str(noise*1000)[:4]+" mV", "l")
     legend.AddEntry(graph_pedestal, "Pedestal: "+str(pedestal*1000)[:4]+" mV", "l")
-    legend.AddEntry(graph_threshold, "Noise: "+str(noise*1000)[:4]+" mV", "l")
     legend.AddEntry(graph_threshold, "Threshold: "+str(threshold*1000)[:5]+" mV", "l")
-    legend.AddEntry(charge_fill, "Charge: "+str(charge*10**15)[:5]+" fC", "l")
-    legend.AddEntry(graph_max_sample, "Max sample: "+str(max_sample*1000)[:5]+" mV", "l")
-    legend.AddEntry(graph_waveform, "Points above threshold: "+str(point_count), "l")
-    legend.AddEntry(graph_90, "10% and 90% limit", "l")
+    #legend.AddEntry(graph_max_sample, "Max sample: "+str(max_sample*1000)[:5]+" mV", "l")
+    #legend.AddEntry(graph_waveform, "Points above threshold: "+str(point_count), "l")
+    legend.AddEntry(graph_peak_value, "Pulse amplitude: "+str(peak_value[0]*1000)[:5]+" mV", "l")
+    legend.AddEntry(graph_peak_time, "Time at peak: " + str(peak_time[0])[0:4] + " ns", "l")
+    legend.AddEntry(graph_linear_fit, "Rise time: "+str(rise_time*1000)[:5]+" ps", "l")
+    #legend.AddEntry(graph_90, "10% and 90% limit", "l")
+    legend.AddEntry(graph_cfd, "CFD0.5: " + str(cfd)[0:4] + " ns", "l")
+    legend.AddEntry(charge_fill, "Charge: "+str(charge*10**15)[:5]+" fC", "f")
+
 
 
     # Define the titles and draw the graph
@@ -231,14 +240,16 @@ def printWaveform(batchNumber, sensor, event = 0):
 
     # Set ranges on axis
     multi_graph.GetYaxis().SetRangeUser(-30,350)
-    #multi_graph.GetXaxis().SetRangeUser(cfd-1,cfd+2.5) # This centers the canvas around the cfd time location point
-    multi_graph.GetXaxis().SetRangeUser(0,100)
+    multi_graph.GetXaxis().SetRangeUser(cfd-1,cfd+2.5) # This centers the canvas around the cfd time location point
+    #multi_graph.GetXaxis().SetRangeUser(0,100)
 
 
     # Export the PDF file
-    fileName = dm.getSourceFolderPath() + dm.getPlotsSourceFolder()+"/waveforms/waveform"+"_"+str(md.getBatchNumber())+"_"+str(runNumber)+"_event_"+str(event)+"_"+str(sensor)+".pdf"
+    fileName = dm.getPlotsSourceFolder()+"/waveforms/waveform"+"_"+str(md.getBatchNumber())+"_"+str(runNumber)+"_event_"+str(event)+"_"+str(sensor)+".pdf"
 
     canvas.Print(fileName)
+
+    print "PDF Produced at", fileName+"."
 
 
 # Print the graph 'number_of_plots' times