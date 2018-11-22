d23d593b8111c25f08bffc6a0bd65e1167797dc8
Antek Szadaj <antek.szadaj@cern.ch>
2018-11-23 00:30:11 +0100

    update convertOscRawToRootTree

--- supplements/convertOscRawToRootTree.C
+++ supplements/convertOscRawToRootTree.C
@@ -1,22 +1,24 @@
 // This is a code which converts the RAW oscilloscope data into .tree.root file to be
 // used for the analysis code. Is uses sdd_mmap.c (no idea where it actually comes from, Nikola M. probably)
 
-// this has to be run in LXPLUS with the command
+// The code has to be run in LXPLUS with the command
 
-// >> root -L convertOscRawToRootTree.C+
+// $ root -L convertOscRawToRootTree.C+
 
 // Change the input variable for the function combinedNtuple to match the correct directory
 // in combinedNtuple.C, a line has been commented out, check for further details
 
 // Additionally, to run this code, lxplus has to be setup to run with
 //
-// root 5.34.25-x86_64-slc6-gcc48-opt with the command
+// $ root 5.34.25-x86_64-slc6-gcc48-opt with the command
 //
-// >> setupATLAS
-// >> localSetupROOT 5.34.25-x86_64-slc6-gcc48-opt
+// $ setupATLAS
 //
-// For me, I had to configure this twice to make it work, be sure to not have any errors when
-// configuring this.
+// $ localSetupROOT 5.34.25-x86_64-slc6-gcc48-opt
+//
+// For me, I had to redo the last two steps to be properly configured.
+// Be sure to not have any errors during configuringation.
+//
 
 {
     
